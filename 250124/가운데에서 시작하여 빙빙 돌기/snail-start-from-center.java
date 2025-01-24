fbab16930ebfb109d06660acfd08658c0b31a7f4
hanong0321 <gksdmd123@gmail.com>
2025-01-24 13:28:25 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 211ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 250124/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_L = 100;
+
+    public static int N;
+    public static int [][] arr = new int[MAX_L][MAX_L];
+    public static int x, y;
+    // 서 북 동 남
+    public static int [] dx = { 0, -1, 0, 1 };
+    public static int [] dy = { -1, 0, 1, 0 };
+    public static int dirNum = 0;
+
+    public static boolean inRange(int x, int y) {
+        return (x >= 0 && x < N) && (y >= 0 && y < N);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        x = N-1; y = N-1;
+        arr[x][y] = N*N;
+        for(int i = N*N - 1; i > 0; i--) {
+            while(true) {
+                int nx = x + dx[dirNum], ny = y + dy[dirNum];
+                if (inRange(nx, ny) && arr[nx][ny] == 0) {
+                    x = nx;
+                    y = ny;
+                    arr[x][y] = i;
+                    break;
+                } else {
+                    dirNum = (dirNum + 1) % 4;
+                }
+            }
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
