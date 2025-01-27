a9c1cbb5466b902165a8b98767f07df113a62197
hanong0321 <gksdmd123@gmail.com>
2025-01-27 17:57:59 +0900

    Title: 체크판위에서 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250127/체크판위에서 2/on-the-checkboard-2.java
+++ 250127/체크판위에서 2/on-the-checkboard-2.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 import static java.lang.Math.abs;
 
 public class Main {
-    public static final int MAX_R  = 5;
-    public static final int MAX_C  = 15;
+    public static final int MAX_L = 15;
 
-    public static char [][] arr = new char[MAX_R][MAX_C];
+    public static char[][] arr = new char[MAX_L][MAX_L];
     public static int R, C;
     public static int x, y = 0;
     public static int cnt = 0;
@@ -16,13 +15,13 @@ public class Main {
         R = sc.nextInt();
         C = sc.nextInt();
 
-        for(int i = 0; i < R; i++) {
-           for(int j = 0; j < C; j++)
-               arr[i][j] = sc.next().charAt(0);
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++)
+                arr[i][j] = sc.next().charAt(0);
         }
 
-        for(int i = x + 1; i < R - 2; i++) {
-            for(int j = y + 1; j < C - 2; j++) {
+        for (int i = x + 1; i < R - 2; i++) {
+            for (int j = y + 1; j < C - 2; j++) {
                 if ((arr[x][y] != arr[i][j])) {
                     for (int k = i + 1; k < R - 1; k++) {
                         for (int l = j + 1; l < C - 1; l++) {
