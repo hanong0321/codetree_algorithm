f05e5b3ffd2872dc86f7cf2ea4c3632047425343
hanong0321 <gksdmd123@gmail.com>
2025-01-27 18:54:32 +0900

    Title: 이상한 진수 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250127/이상한 진수 2/awkward-digits-2.java
+++ 250127/이상한 진수 2/awkward-digits-2.java
@@ -3,19 +3,15 @@ import java.util.Scanner;
 import static java.lang.Math.abs;
 
 public class Main {
-    public static int INT_MIN = Integer.MIN_VALUE;
+    public static int INT_MIN = Integer.MIN_VALUE;;
 
-    public static String a;
     public static int maxSum = INT_MIN;
+    public static String a;
 
     public static String change(int idx, char c) {
         StringBuilder sb = new StringBuilder(a);
         sb.setCharAt(idx, c);
         return sb.toString();
-    }
-
-    public static int cal(String str) {
-        return Integer.parseInt(str, 2);
     }
 
     public static void main(String[] args) {
@@ -28,7 +24,7 @@ public class Main {
             int sum = 0;
             if(a.charAt(i) == '0') {
                 modified = change(i, '1');
-                sum = cal(modified);
+                sum = Integer.parseInt(modified, 2);
             }
             maxSum = Math.max(maxSum, sum);
         }
